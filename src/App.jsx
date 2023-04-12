b21dfc074f0ada83c21b6cbc1235fc6f33efe60e
Pratik Neupane <pratikneupane1999@gmail.com>
2023-04-12 15:56:53 +0545

    fix: props passing to Questions components and minor css changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import Question from "./components/Question";
 import questions from "./data";
 function App() {
   const [data, setData] = useState(questions);
-
+  console.log("APp");
+  console.log(data);
   return (
-    <main className="h-screen bg-gray-800 flex justify-center items-center ">
-      {data.map((list) => {
-        <Question ques={list} />;
-      })}
+    <main className="h-screen bg-gray-800 flex flex-col justify-center items-center ">
+      <div className="h-fit w-6/12 bg-white rounded-md shadow-md  p-3">
+        {data.map((list) => {
+          console.log("map");
+          console.log(list);
+          return <Question ques={list} />;
+        })}
+      </div>
     </main>
   );
 }
